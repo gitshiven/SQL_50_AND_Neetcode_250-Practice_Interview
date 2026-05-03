a887b74d2a9459afe4724e65c2f76dcf2f27a2be
Shiven Singh <ss.shiven44@gmail.com>
2026-05-03 12:59:06 +0100

    Time: 1350 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,7 +1,7 @@
 SELECT ROUND(AVG(order_date = customer_pref_delivery_date) * 100, 2) AS immediate_percentage
-FROM Delivery
-WHERE (customer_id, order_date) IN (
-    SELECT customer_id, MIN(order_date)
+FROM Delivery d
+WHERE order_date = (
+    SELECT MIN(order_date)
         FROM Delivery
-            GROUP BY customer_id
+            WHERE customer_id = d.customer_id
             );
