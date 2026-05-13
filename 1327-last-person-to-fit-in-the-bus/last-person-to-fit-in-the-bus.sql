dad00c08ec9d1f46356abb1e4d2eace4fc5a8bd2
Shiven Singh <ss.shiven44@gmail.com>
2026-05-13 01:37:28 +0100

    Time: 846 ms (43.56%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,27 @@
+WITH cte AS (
+    SELECT
+        person_name,
+        turn,
+        SUM(weight) OVER (
+            ORDER BY turn
+        ) AS running_weight
+    FROM Queue
+)
+
+SELECT person_name
+FROM cte
+WHERE running_weight <= 1000
+ORDER BY running_weight DESC
+LIMIT 1;
+
+#Window Function   --Interesting
+
+
+#SELF_JOIN
+-- SELECT 
+--     q1.person_name
+-- FROM Queue q1 JOIN Queue q2 ON q1.turn >= q2.turn
+-- GROUP BY q1.turn
+-- HAVING SUM(q2.weight) <= 1000
+-- ORDER BY SUM(q2.weight) DESC
+-- LIMIT 1
