50edc64493180f49e0bcbb34ef5c99de4a5060c0
Shiven Singh <ss.shiven44@gmail.com>
2026-05-19 13:31:41 +0100

    Time: 720 ms (77.55%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+#REGEX (regulare regex)
+SELECT user_id, name, mail
+FROM users
+WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$' AND mail LIKE BINARY '%@leetcode.com';
+
+#new_concept 
