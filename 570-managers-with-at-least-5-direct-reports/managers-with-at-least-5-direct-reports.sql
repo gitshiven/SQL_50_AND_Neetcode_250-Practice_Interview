d5d2c8377aedcacf8019e98af3b8d08586ed930c
Shiven Singh <ss.shiven44@gmail.com>
2026-07-19 13:10:37 +0100

    Time: 338 ms (86.79%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,9 +1,6 @@
 # Write your MySQL query statement below
-Select name from Employee
-where id IN(
-    Select managerID
-    From Employee
-    Group by ManagerId
-    Having Count(*) >=5
-);
-
+Select e1.name
+FROM Employee e1
+   JOIN Employee e2 ON e1.id = e2.managerId
+   group by e1.id, e1.name
+   having count(e2.managerId) >=5
