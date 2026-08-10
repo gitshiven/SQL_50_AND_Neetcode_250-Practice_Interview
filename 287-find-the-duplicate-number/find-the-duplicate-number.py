f90fde0be01a0f9d2c0ca54cc7b5ac744151d5b6
Shiven Singh <ss.shiven44@gmail.com>
2026-08-10 12:01:31 +0100

    Time: 19 ms (90.54%) | Memory: 33.8 MB (18.65%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -5,3 +5,20 @@ class Solution:
             if i in seen:
                 return i
             seen.add(i)
+
+# Set Internally Kaise Kaam Karta Hai
+# seen = {1, 3, 4, 2}
+
+# Andar yeh hota hai —
+
+# Bucket 0: []
+# Bucket 1: [1]   ← hash(1)=1
+# Bucket 2: [2]   ← hash(2)=2
+# Bucket 3: [3]   ← hash(3)=3
+# Bucket 4: [4]   ← hash(4)=4
+# 2 in seen Check Kaise Hota Hai
+# Step 1: hash(2) = 2
+# Step 2: Bucket 2 pe jao
+# Step 3: 2 hai? YES → return True
+
+# Sirf 1 step → O(1) ✅
